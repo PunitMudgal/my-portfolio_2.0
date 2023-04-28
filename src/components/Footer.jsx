0367690cd156593142606045b7878bff13cf8811
Punit Mudgal <ssmudgal01@gmail.com>
2023-04-28 18:38:12 +0530

    links added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,16 +16,16 @@ function Footer() {
         <div className="footer-content-icons">
           <h4>SOCIAL</h4>
           <div className="footer-social-icons">
-            <a href="">
+            <a href="https://www.instagram.com/punitmudgal_/">
               <FaInstagram size={30} />
             </a>
-            <a href="">
+            <a href="https://www.linkedin.com/in/punit-sharma-343160236">
               <FaLinkedinIn size={30} />
             </a>
-            <a href="">
+            <a href="https://github.com/PunitMudgal">
               <FaGithub size={30} />
             </a>{" "}
-            <a href="">
+            <a href="https://twitter.com/PunitMudgal25">
               <FaTwitter size={30} />
             </a>{" "}
           </div>
